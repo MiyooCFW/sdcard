c5a9bf3d9598038b072d7d8cd63518cd8db4482a
tiopex <tiopxyz@gmail.com>
2022-06-25 22:33:56 +0200

    add bitboy2x variant with st7789s lcd panel

--- gen_boot_scr.sh
+++ gen_boot_scr.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkimage -C none -A arm -T script -d boot.cmd boot/boot.scr
-mkimage -C none -A arm -T script -d boot-bb2x.cmd boot/variants/bittboy2x/boot.scr
+mkimage -C none -A arm -T script -d boot-bb2x.cmd boot/variants/bittboy2x_v1/boot.scr
+mkimage -C none -A arm -T script -d boot-bb2x.cmd boot/variants/bittboy2x_v2/boot.scr
 mkimage -C none -A arm -T script -d boot-bb35.cmd boot/variants/bittboy3.5/boot.scr
 mkimage -C none -A arm -T script -d boot-pocketgo-v90-q90.cmd boot/variants/v90_q90/boot.scr
 mkimage -C none -A arm -T script -d boot-m3.cmd boot/variants/m3/boot.scr
