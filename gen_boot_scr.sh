978d07385cc2e777c37e687543cd75dadc41300c
Apaczer <94932128+Apaczer@users.noreply.github.com>
2022-10-14 00:33:36 +0200

    add new v90 keyboard layout

--- gen_boot_scr.sh
+++ gen_boot_scr.sh
@@ -4,5 +4,6 @@ mkimage -C none -A arm -T script -d boot-bb2x.cmd boot/variants/bittboy2x_v1/boo
 mkimage -C none -A arm -T script -d boot-bb2x.cmd boot/variants/bittboy2x_v2/boot.scr
 mkimage -C none -A arm -T script -d boot-bb35.cmd boot/variants/bittboy3.5/boot.scr
 mkimage -C none -A arm -T script -d boot-pocketgo-v90-q90.cmd boot/variants/v90_q90/boot.scr
+mkimage -C none -A arm -T script -d boot-v90_v2.cmd boot/variants/v90_v2/boot.scr
 mkimage -C none -A arm -T script -d boot-m3.cmd boot/variants/m3/boot.scr
 mkimage -C none -A arm -T script -d boot-xyc.cmd boot/variants/xyc/boot.scr
