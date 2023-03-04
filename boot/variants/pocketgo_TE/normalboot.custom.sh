f27954de04066259f9fe8a08f2c17d9bb32325ed
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-03-04 14:44:08 +0100

    remove redundant boot/variants dir
    - rm all variants
    - move daemon to /boot
    - move boot-logo & update to Miyoo logo
    - add generic scripts to /boot

--- boot/variants/pocketgo_TE/normalboot.custom.sh
+++
@@ -1,7 +0,0 @@
-# expected to be run from this directory
-# LOGS variable is set, but it should usually be /dev/null
-# CONSOLE_VARIANT is also set
-#
-#./miyooctl setversion 3 > /dev/null 2>&1
-./daemon >> "${LOGS}" 2>&1
-
