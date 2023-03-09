2db2a6f757b12556c1d911af1cd0d3dfd5688256
tiopex <tiopxyz@gmail.com>
2023-03-09 12:12:43 +0100

    add scripts to change USB mode fix boot.scr generations

--- gen_boot_scr.sh
+++ gen_boot_scr.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkimage -C none -A arm -T script -d boot.cmd boot/boot.scr
-mkimage -C none -A arm -T script -d boot-1bit.cmd boot/boot-1bit.scr
+mkimage -C none -A arm -T script -d boot-1bit.cmd boot/boot-4bit.scr
 mkimage -C none -A arm -T script -d boot-test.cmd boot/boot-test.scr
 # bootcmd_args in boot.cmd are variables set automatic by u-boot at launch
 # you can pass your own values and generate custom boot.scr (see example in boot-test.cmd)
