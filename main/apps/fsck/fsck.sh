cc773b7e94f04871c889ab8069944d0c2834abce
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-03-12 00:48:17 +0100

    update st-sdl app & fsck script

--- main/apps/fsck/fsck.sh
+++ main/apps/fsck/fsck.sh
@@ -17,6 +17,12 @@ if dmesg | grep "mmcblk0p4"; then
 echo -e "\e[31mUnclean shutdown detected.\e[0m"
 echo -e "\e[32mChecking FAT32 (MAIN) partition...\e[0m" 
 fsck.vfat -a /dev/mmcblk0p4
+echo " "
+echo "Rebooting device, please wait..."
+echo " "
+sleep 2
+sync
+reboot
 fi
 
 echo "Finished!"
